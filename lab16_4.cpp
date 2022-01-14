2c0b7592c54d0e0ad89f73a9c6bdbf68f28dccc4
ChonlananT <ciwerxil@gmail.com>
2022-01-14 17:27:59 +0700

    lab16_4.cpp completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,32 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+    int temp, m;
+	int x[] = {a,b,c,d};
+    int j = 4;
+	for(int i = 0 ; i < 4; i++){
+        m = rand()%j;
+        if(m == 1){
+            temp = x[i];
+            x[i] = x[i+1];
+            x[i+1] = temp;
+        }
+        else if(m == 2){
+            temp = x[i];
+            x[i] = x[i+2];
+            x[i+2] = temp;
+        }
+        else if(m == 3){
+            temp = x[i];
+            x[i] = x[i+3];
+            x[i+3] = temp;
+        }
+		j--;
+    }
+    a = x[0];
+    b = x[1];
+    c = x[2];
+    d = x[3];
+}
