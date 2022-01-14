b77a8e6fd784a1c51f8a3719bd69c2e40b19ef9b
ChonlananT <ciwerxil@gmail.com>
2022-01-14 17:34:55 +0700

    lab16_5.cpp completed

--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,19 +3,46 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *,int *,int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
+ 	
+	srand(time(0)); 
+		
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
-	    cout << a << " " << b << " " << c << " " << d << "\n";
-	}
+     	shuffle(&a,&b,&c,&d); 
+     	cout << a << " " << b << " " << c << " " << d << "\n";
+ 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d){
+    int temp, m;
+	int x[] = {*a,*b,*c,*d};
+	int j = 4;
+    for(int i = 0 ; i < 4; i++){
+        m = rand()%j;
+        if(m == 1){
+            temp = x[i];
+            x[i] = x[i+1];
+            x[i+1] = temp;
+        }
+        else if(m == 2){
+            temp = x[i];
+            x[i] = x[i+2];
+            x[i+2] = temp;
+        }
+        else if(m == 3){
+            temp = x[i];
+            x[i] = x[i+3];
+            x[i+3] = temp;
+        }
+		j--;
+    }
+    *a = x[0];
+    *b = x[1];
+    *c = x[2];
+    *d = x[3];
+}
